f98cbbdc0a9decd1430d0337188598e32087f24c
Abhay Patel <apatelcs108@gmail.com>
2023-04-15 17:51:06 -0400

    Hello world using piston + Added utils.rs file for utility functioins (drawing shapes) ~ Replaced main function in main.rs with piston hello world example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate piston_window;
+
+use piston_window::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut window: PistonWindow = WindowSettings::new("Hello World", [512; 2]).exit_on_esc(true).build().unwrap(); 
+
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |c, g, _| {
+            clear([0.5, 0.5, 0.5, 1.0], g);
+            rectangle([1.0, 0.0, 0.0, 1.0], // red
+                      [0.0, 0.0, 100.0, 100.0], // rectangle
+                      c.transform, g);
+        });
+    }
 }
