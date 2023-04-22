36e2a0f1c6124ea9dfbbabca813d98fb2121ca98
Abhay Patel <apatelcs108@gmail.com>
2023-04-21 21:22:04 -0400

    Updated utils and main + Added Paddle class with init function and draw method + Added Ball class with init function and draw method - Commented out draw_player and draw_ball methods (TO BE REMOVED FULLY NEXT COMMIT) ~ Changed main.rs to use Paddle and Ball structs

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,22 @@ extern crate piston_window;
 mod utils;
 
 use piston_window::*;
-use utils::*;
+use utils::{Paddle, Ball};
 
 fn main() {
     let mut window: PistonWindow = WindowSettings::new("PONG", [512; 2]).exit_on_esc(true).build().unwrap(); 
+    let mut left_paddle = Paddle::new(10, 0, true);
+    let mut right_paddle = Paddle::new(494, 50, false);
+    let mut ball = Ball::new(200, 150);
 
     while let Some(event) = window.next() {
         window.draw_2d(&event, |c, g, _| {
-            draw_player(10, 0, c, g);
-            draw_player(501, 50, c, g);
-            draw_ball(200, 150, c, g);
+            // draw_player(10, 0, c, g);
+            // draw_player(501, 50, c, g);
+            // draw_ball(200, 150, c, g);
+            left_paddle.draw(&c, g);
+            right_paddle.draw(&c, g);
+            ball.draw(&c, g);
         });
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,16 +1,59 @@
 use piston_window::rectangle;
+use piston_window::ellipse;
 use piston_window::color::WHITE;
 use piston_window::Context;
 use piston_window::G2d;
 
-const PLAYER_WIDTH: f64 = 5 as f64;
-const PLAYER_HEIGHT: f64 = 30 as f64;
-const BALL_WIDTH: f64 = 5 as f64;
+const PADDLE_WIDTH: f64 = 8 as f64;
+const PADDLE_HEIGHT: f64 = 48 as f64;
+const BALL_RAD: f64 = 8 as f64;
 
-pub fn draw_player(sx: i32, sy: i32, con: Context, g: &mut G2d) {
-    rectangle(WHITE, [(sx as f64), (sy as f64), PLAYER_WIDTH, PLAYER_HEIGHT], con.transform, g);
+// pub fn draw_player(sx: i32, sy: i32, con: Context, g: &mut G2d) {
+//     rectangle(WHITE, [(sx as f64), (sy as f64), PLAYER_WIDTH, PLAYER_HEIGHT], con.transform, g);
+// }
+
+// pub fn draw_ball(sx: i32, sy: i32, con: Context, g: &mut G2d) {
+//     rectangle(WHITE, [(sx as f64), (sy as f64), BALL_WIDTH, BALL_WIDTH], con.transform, g);
+// }
+
+pub struct Paddle {
+    pub x: i32,
+    pub y: i32,
+    pub is_player: bool,
 }
 
-pub fn draw_ball(sx: i32, sy: i32, con: Context, g: &mut G2d) {
-    rectangle(WHITE, [(sx as f64), (sy as f64), BALL_WIDTH, BALL_WIDTH], con.transform, g);
+impl Paddle {
+    pub fn new(x: i32, y: i32, is_player: bool) -> Paddle {
+        Paddle {
+            x: x,
+            y: y,
+            is_player: is_player,
+        }
+    }
+
+    pub fn draw(&self, con: &Context, g: &mut G2d) {
+        rectangle(WHITE, [(self.x as f64), (self.y as f64), PADDLE_WIDTH, PADDLE_HEIGHT], con.transform, g);
+    }
+}
+
+pub struct Ball {
+    pub x: i32,
+    pub y: i32,
+    pub x_vel: f64,
+    pub y_vel: f64,
+}
+
+impl Ball {
+    pub fn new(x: i32, y: i32) -> Ball {
+        Ball {
+            x: x,
+            y: y,
+            x_vel: 1.0,
+            y_vel: 0.0,
+        }
+    }
+
+    pub fn draw(&self, con: &Context, g: &mut G2d) {
+        ellipse(WHITE, [(self.x as f64), (self.y as f64), BALL_RAD, BALL_RAD], con.transform, g);
+    }
 }
